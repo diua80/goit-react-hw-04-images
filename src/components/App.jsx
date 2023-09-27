5df0c6aac0c85046afbffd80e108134e54b02b33
Iryna Yatsenko <iryna.didova@gmail.com>
2023-09-27 12:37:55 +0300

    image finder done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,46 @@
-import React, { useState, useEffect } from 'react';
 import { SearchBar } from './searchBar/Searchbar';
 import { ImageGallery } from './imageGallery/ImageGallery';
 import { Loader } from './loader/Loader';
 import { getImages } from '../api';
 import { Button } from './button/Button';
 import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
+  const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
-  const [query, setQuery] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [loadedImgCount, setLoadedImgCount] = useState(0);
-  const [randomId, setRandomId] = useState(nanoid());
+  const [randomId, setRandomId] = useState(0);
+
+const handleQueryChange = newQuery => {
+  setQuery(newQuery);
+  setImages([]);
+  setPage(1);
+  setRandomId(nanoid());
+  }
 
   useEffect(() => {
-    if (query) {
-      fetchData()
-    };
-  }, [query, page, randomId]);
-
-  const fetchData = async () => {
-    setIsLoading(true);
-    try {
-      const result = query.slice(query.indexOf('/') + 1);
-      const data = await getImages(result, page);
-
-      setImages(prevImages => [...prevImages, ...data.hits]);
-      setLoadedImgCount(Math.ceil(data.totalHits / 12));
-    } catch (error) {
-      console.error("Помилка при отриманні даних:", error);
-    } finally {
-      setIsLoading(false);
+    if (query === "") return;
+    fetchedImages();
+    async function fetchedImages() {
+      try {
+        setIsLoading(true);
+        const result = query.slice(query.indexOf('/') + 1);
+        const data = await getImages(result, page);
+        setImages([...images, ...data.hits]);
+        setLoadedImgCount(Math.ceil(data.totalHits / 12));
+      } catch (error) {
+        console.error("Помилка при отриманні даних:", error);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  };
+  }, [page, query, randomId]);
 
   const handleLoadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  const handleQueryChange = newQuery => {
-    setQuery(newQuery);
-    setImages([]);
-    setPage(1);
-    setRandomId(nanoid());
+    setPage(prevState => (prevState + 1));
   };
 
   return (
@@ -55,11 +52,11 @@ export const App = () => {
         <ImageGallery images={images} />
       </div>
       <div>
-        {loadedImgCount > 0 && loadedImgCount !== page && (
-          <Button onHandleLoadMore={handleLoadMore} />
-        )}
+        {loadedImgCount > 0 && loadedImgCount !== page && <Button onHandleLoadMore={handleLoadMore} />}
       </div>
-      <div>{isLoading && <Loader />}</div>
+      <div>
+        {isLoading && <Loader />}
+      </div>
     </div>
   );
-};
+}
